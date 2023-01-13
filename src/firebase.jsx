094fbcc208d842d16700bbba1cfdb0d9b42c5b9b
Morsetim <mauricium.maurice@yahoo.com>
2023-01-13 05:16:31 +0100

    setup firebase

---
+++ src/firebase.jsx
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCRpv26Q3JL0meS2AR2MuHwZKrCwoTukfM",
+  authDomain: "parabellum-81b69.firebaseapp.com",
+  projectId: "parabellum-81b69",
+  storageBucket: "parabellum-81b69.appspot.com",
+  messagingSenderId: "30380897537",
+  appId: "1:30380897537:web:aa1ccee04bceb0a1531700",
+  measurementId: "G-FWSKZPN3HX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
